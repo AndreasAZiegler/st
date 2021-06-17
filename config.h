4f9fb8f4ba6bc1ca2e24243aeac4f75a78002807
Andreas Ziegler <062.127@gmail.com>
2021-06-17 15:32:35 +0200

    Changed font size, switched to solarized-light and fixed background.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=18:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
@@ -113,23 +113,23 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* solarized dark */
-	"#073642",  /*  0: black    */
+	/* solarized light */
+	"#eee8d5",  /*  0: black    */
 	"#dc322f",  /*  1: red      */
 	"#859900",  /*  2: green    */
 	"#b58900",  /*  3: yellow   */
 	"#268bd2",  /*  4: blue     */
 	"#d33682",  /*  5: magenta  */
 	"#2aa198",  /*  6: cyan     */
-	"#eee8d5",  /*  7: white    */
-	"#002b36",  /*  8: brblack  */
+	"#073642",  /*  7: white    */
+	"#fdf6e3",  /*  8: brblack  */
 	"#cb4b16",  /*  9: brred    */
-	"#586e75",  /* 10: brgreen  */
-	"#657b83",  /* 11: bryellow */
-	"#839496",  /* 12: brblue   */
+	"#93a1a1",  /* 10: brgreen  */
+	"#839496",  /* 11: bryellow */
+	"#657b83",  /* 12: brblue   */
 	"#6c71c4",  /* 13: brmagenta*/
-	"#93a1a1",  /* 14: brcyan   */
-	"#fdf6e3",  /* 15: brwhite  */
+	"#586e75",  /* 14: brcyan   */
+	"#002b36",  /* 15: brwhite  */
 };
 
 
@@ -139,9 +139,9 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 12;
 unsigned int defaultbg = 8;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 14;
-unsigned int background = 15;
+unsigned int defaultcs = 14;
+unsigned int defaultrcs = 15;
+unsigned int background = 8;
 
 /*
  * Default shape of cursor
